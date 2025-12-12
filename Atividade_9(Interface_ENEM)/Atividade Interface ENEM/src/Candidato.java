74d8ea61ec113d7144fd46cec9a2592208f66eb1
GuilhermeAntonique <guilhermeantonique575.2@gmail.com>
2025-12-12 16:44:07 -0300

    Atividade Interface ENEM

---
+++ Atividade_9(Interface_ENEM)/Atividade Interface ENEM/src/Candidato.java
@@ -0,0 +1,4 @@
+public class Candidato implements Comparable<Candidato> {
+    /*As interfaces são extremamente importantes na programação, pois facilita tarefas que devem ser criadas a partir do momento que te permite utilizar funções pré-existentes que outras pessoas criaram.
+    Uma interface em específico me ajudou muito neste código. Esta interface é a Comparable, pois ela que permitiu a utilização da função Collections.sort, que organizou os candidatos.*/
+}
